cc53bde7db5dbedbb631c19cb4e51e9a6750ccd1
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-04-28 20:48:58 +0100

    Created TaskSelectField and related enum needed

--- src/components/taskForm/CreateTaskForm.tsx
+++ src/components/taskForm/CreateTaskForm.tsx
@@ -3,6 +3,9 @@ import { FC, ReactElement } from 'react';
 import { Box, Typography, Stack } from '@mui/material';
 import TaskInputField from './TaskInputField';
 import TaskDateField from './TaskDateField';
+import TaskSelectField from './TaskSelectField';
+import { Status } from './enums/Status';
+import { Priority } from './enums/Priority';
 
 const CreateTaskForm: FC = (): ReactElement => {
   return (
@@ -33,6 +36,25 @@ const CreateTaskForm: FC = (): ReactElement => {
           multiline
         />
         <TaskDateField />
+        <Stack direction="row" spacing={2} width="100%">
+          <TaskSelectField
+            label="Status"
+            name="status"
+            options={[
+              { value: Status.TODO, label: Status.TODO },
+              { value: Status.IN_PROGRESS, label: Status.IN_PROGRESS },
+            ]}
+          />
+          <TaskSelectField
+            label="Priority"
+            name="priority"
+            options={[
+              { value: Priority.LOW, label: Priority.LOW },
+              { value: Priority.NORMAL, label: Priority.NORMAL },
+              { value: Priority.HIGH, label: Priority.HIGH },
+            ]}
+          />
+        </Stack>
       </Stack>
     </Box>
   );
