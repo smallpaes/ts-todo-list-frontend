6627c4a9d8dcbf528d5a63239d0f5f69a02cb043
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-05-02 20:29:15 +0100

    Added react query and dev tool

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { FC, ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { customTheme } from './theme/customTheme';
 import Dashboard from './pages/dashboard/Dashboard';
 
+const queryClient = new QueryClient();
+
 const App: FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <Dashboard />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
